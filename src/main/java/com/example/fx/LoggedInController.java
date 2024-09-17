fd36da8ba79a78f5c8cba94a9dc4f05fc061f9d0
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2024-09-17 11:07:52 +0200

    updated loggedin controller

--- src/main/java/com/example/fx/LoggedInController.java
+++ src/main/java/com/example/fx/LoggedInController.java
@@ -2,11 +2,20 @@ package com.example.fx;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class LoggedInController {
     @FXML
     private Button button_switch_to_reg;
+    @FXML
+    private TextField tf_email;
+    @FXML
+    private TextField tf_password;
+    @FXML
+    private Button button_login;
+
 
     @FXML
     public void initialize() {
@@ -14,5 +23,15 @@ public class LoggedInController {
         button_switch_to_reg.setOnAction((ActionEvent event) -> {
             DBUtils.changeScene(event, "sign-up.fxml", null);
         });
+    }
+    public void Result(ActionEvent event){
+        String email = tf_email.getText();
+        String password = tf_password.getText();
+        if(email.isEmpty() || password.isEmpty()){
+            DBUtils.showAlert(Alert.AlertType.ERROR, "Error", "All fields must be filled out");
+            return;
+        }
+        DBUtils dbUtils = new DBUtils();
+        dbUtils.SignInUser(event, email,password);
     }
 }
