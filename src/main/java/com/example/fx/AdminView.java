8ccad5481e5ed1c264ed3879b07cd97f94544e40
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2024-09-18 15:00:07 +0200

    initialized properties

--- src/main/java/com/example/fx/AdminView.java
+++ src/main/java/com/example/fx/AdminView.java
@@ -1,4 +1,59 @@
 package com.example.fx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
 public class AdminView {
+    @FXML
+    private TableColumn<?, ?> admin_student_country;
+
+    @FXML
+    private TableColumn<?, ?> admin_student_course;
+
+    @FXML
+    private TableColumn<?, ?> admin_student_date;
+
+    @FXML
+    private TableColumn<?, ?> admin_student_gender;
+
+    @FXML
+    private TableColumn<?, ?> admin_student_name;
+
+    @FXML
+    private TableColumn<?, ?> admin_student_number;
+
+    @FXML
+    private TableColumn<?, ?> admin_student_status;
+
+    @FXML
+    private TableColumn<?, ?> admin_student_surname;
+
+    @FXML
+    private Button btn_add;
+
+    @FXML
+    private Button btn_add_course;
+
+    @FXML
+    private Button btn_add_dashboard;
+
+    @FXML
+    private Button btn_add_salalry;
+
+    @FXML
+    private Button btn_add_student;
+
+    @FXML
+    private Button btn_add_subject;
+
+    @FXML
+    private Button btn_add_teacher;
+
+    @FXML
+    private Button btn_delete;
+
+    @FXML
+    private Button btn_update;
+
 }
