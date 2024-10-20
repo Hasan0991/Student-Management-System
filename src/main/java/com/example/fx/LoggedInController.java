eb5b0a21bf85c8df85798abb1915d0b6b6b71b17
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2024-10-20 14:08:44 +0200

    :wq

--- src/main/java/com/example/fx/LoggedInController.java
+++ src/main/java/com/example/fx/LoggedInController.java
@@ -19,7 +19,7 @@ public class LoggedInController {
 
     @FXML
     public void initialize() {
-        // When the sign-up button is pressed, switch to sign-up scene
+
         button_switch_to_reg.setOnAction((ActionEvent event) -> {
             DBUtils.changeScene(event, "sign-up.fxml", null);
         });
