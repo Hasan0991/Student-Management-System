efe60adf87e23a2a0a22a4e27a5fb0675d21739f
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2024-10-26 13:59:38 +0200

    created class to update info of student

--- src/main/java/com/example/fx/UpdateStudentInfo.java
+++ src/main/java/com/example/fx/UpdateStudentInfo.java
@@ -1,4 +1,17 @@
 package com.example.fx;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
 public class UpdateStudentInfo {
+
+    @FXML
+    private ComboBox<?> update_student_combox;
+
+    @FXML
+    private Button update_student_save_btn;
+
+    add_student_course.getItems().addAll("Course 1", "Course 2", "Course 3");
+
 }
