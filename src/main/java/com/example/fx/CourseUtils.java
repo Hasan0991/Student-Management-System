637a171977337d075edec418945bb055be9cc94c
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2024-10-26 14:13:22 +0200

    added methods to store info for differet combobxes

--- src/main/java/com/example/fx/CourseUtils.java
+++ src/main/java/com/example/fx/CourseUtils.java
@@ -6,4 +6,10 @@ public class CourseUtils {
     public static void populateCourseComboBox(ComboBox<String> comboBox) {
         comboBox.getItems().addAll("Course 1", "Course 2", "Course 3");
     }
+    public static void populateGender(ComboBox<String> comboBox) {
+        comboBox.getItems().addAll("Male", "Female", "Other");
+    }
+    public static void populateStartedYear(ComboBox<String> comboBox) {
+        comboBox.getItems().addAll("2021", "2022", "2023","2024");
+    }
 }
