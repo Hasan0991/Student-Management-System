bedc395d17221dbc118cd5ac19ea51270800c6b7
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2024-10-26 16:37:59 +0200

    created method to replace student data .is not workable

--- src/main/java/com/example/fx/UpdateStudentInfo.java
+++ src/main/java/com/example/fx/UpdateStudentInfo.java
@@ -1,6 +1,8 @@
 package com.example.fx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 
@@ -16,5 +18,18 @@ public class UpdateStudentInfo {
     public void initialize() {
         CourseUtils.populateCourseComboBox(update_student_combox);
     }
+    @FXML
+    public void onAction1(ActionEvent event,int student_id) {
+        System.out.println("i am here");
+        DBUtils db = new DBUtils();
+        boolean flag = db.replace_studentData(student_id, update_student_combox.getValue());
+        if (flag){
+            DBUtils.showAlert(Alert.AlertType.CONFIRMATION,"Congrutulations","you have replaced data corerctly");
+        }
+        else {
+            DBUtils.showAlert(Alert.AlertType.ERROR,"ERROR", "something went wrong");
+        }
+    }
+
 
 }
