279a792b64323360f21ca91124a1591dfbdc2543
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2024-10-31 12:14:33 +0100

    created method intialize to store studentID taken from admin-view

--- src/main/java/com/example/fx/UpdateStudentInfo.java
+++ src/main/java/com/example/fx/UpdateStudentInfo.java
@@ -7,9 +7,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 
 public class UpdateStudentInfo {
-
+    private int studentId;
     @FXML
     private ComboBox<String> update_student_combox;
+
 
     @FXML
     private Button update_student_save_btn;
@@ -18,13 +19,18 @@ public class UpdateStudentInfo {
     public void initialize() {
         CourseUtils.populateCourseComboBox(update_student_combox);
     }
+
+    public void initializeStudentId(int studentId) {
+        this.studentId = studentId;
+    }
     @FXML
-    public void onAction1(ActionEvent event,int student_id) {
+    public void onSave(ActionEvent event) {
         System.out.println("i am here");
         DBUtils db = new DBUtils();
-        boolean flag = db.replace_studentData(student_id, update_student_combox.getValue());
+        boolean flag = db.replace_studentData(studentId, update_student_combox.getValue());
         if (flag){
-            DBUtils.showAlert(Alert.AlertType.CONFIRMATION,"Congrutulations","you have replaced data corerctly");
+            DBUtils.showAlert(Alert.AlertType.CONFIRMATION,"Congrutulations","you have replaced data corectly");
+            DBUtils.changeScene(event,"admin-view.fxml","null");
         }
         else {
             DBUtils.showAlert(Alert.AlertType.ERROR,"ERROR", "something went wrong");
