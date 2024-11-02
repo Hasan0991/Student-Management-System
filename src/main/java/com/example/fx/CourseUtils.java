991958f6c4e949e902d02f2ac67e99e994728c09
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2024-11-02 15:13:52 +0100

    changed method to retrieve data from course table and show it in combobox

--- src/main/java/com/example/fx/CourseUtils.java
+++ src/main/java/com/example/fx/CourseUtils.java
@@ -2,14 +2,35 @@ package com.example.fx;
 
 import javafx.scene.control.ComboBox;
 
+import java.sql.*;
+
 public class CourseUtils {
+
     public static void populateCourseComboBox(ComboBox<String> comboBox) {
-        comboBox.getItems().addAll("Computer Science", "Computer Systems", "Economics");
+        String query = "SELECT course_name FROM course";
+
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/javafx", "root", "hasan099");
+             PreparedStatement stmt = conn.prepareStatement(query);
+             ResultSet rs = stmt.executeQuery()) {
+
+            comboBox.getItems().clear();
+            while (rs.next()) {
+                String courseName = rs.getString("course_name");
+                comboBox.getItems().add(courseName);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
     public static void populateGender(ComboBox<String> comboBox) {
+        comboBox.getItems().clear();
         comboBox.getItems().addAll("Male", "Female", "Other");
     }
+
     public static void populateStartedYear(ComboBox<String> comboBox) {
-        comboBox.getItems().addAll("2021", "2022", "2023","2024");
+        comboBox.getItems().clear();
+        comboBox.getItems().addAll("2021", "2022", "2023", "2024");
     }
 }
